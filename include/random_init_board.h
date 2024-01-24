26ee712f7df4e180b225805fa6aa7f0d2704e2a0
ickule <117572745+ickule@users.noreply.github.com>
2024-01-24 22:51:34 +0100

    :bulb: Improved comment accuracy in random_init_board

--- include/random_init_board.h
+++ include/random_init_board.h
@@ -5,6 +5,6 @@
 #include <time.h>
 
 void random_init_board(int *board, int rows, int cols);
-/* Initialise the board with all cells at 0 */
+/* Initialise the board with a random dead or alive state */
 
 #endif /* BOARD_H_INCLUDED */
