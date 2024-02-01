96eb03f56e9dc40a2b85571882f675ffd410127b
ickule <117572745+ickule@users.noreply.github.com>
2024-02-01 21:40:26 +0100

    :recycle: Refactor count_neighbours to improve readability

--- src/count_neighbours.c
+++ src/count_neighbours.c
@@ -1,102 +1,17 @@
 #include "count_neighbours.h"
 
-int count_neighbours(int x, int y, int *board, int rows, int cols)
-/* Return the number of neighbours of the (x,y) point */
-{
+int count_neighbours(int x, int y, int *board, int rows, int cols) {
   int neighbours = 0;
-  if (x == 0 && y == 0) {
-    /* x = 0 and y = 0 so no (x - 1) nor (y - 1) */
-    // clang-format off
-    neighbours =
-      board[x * cols + (y + 1)] +
-      board[(x + 1) * cols + y] +
-      board[(x + 1) * cols + (y + 1)];
-    // clang-format on
+
+  /* Iterate over neighbouring cells in a 3x3 grid around the current cell (x, y) */
+  for (int i = x - 1; i <= x + 1; i++) {
+    for (int j = y - 1; j <= y + 1; j++) {
+      /* Check if the neighbouring cell is within the bounds of the board */
+      if (i >= 0 && i < rows && j >= 0 && j < cols && !(i == x && j == y)) {
+        neighbours += board[i * cols + j];
+      }
+    }
   }
-  if (x == 0 && y > 0 && y < (cols - 1)) {
-    /* x = 0 and y > 0 < cols so no (x - 1) */
-    // clang-format off
-    neighbours =
-      board[x * cols + (y - 1)] +
-      board[x * cols + (y + 1)] +
-      board[(x + 1) * cols + (y - 1)] +
-      board[(x + 1) * cols + y] +
-      board[(x + 1) * cols + (y + 1)];
-    // clang-format on
-  }
-  if (x == 0 && y == (cols - 1)) {
-    /* x = 0 and y = cols so no (x - 1) nor (y + 1) */
-    // clang-format off
-    neighbours =
-      board[x * cols + (y - 1)] +
-      board[(x + 1) * cols + (y - 1)] +
-      board[(x + 1) * cols + y];
-    // clang-format on
-  }
-  if (x > 0 && x < (rows - 1) && y == 0) {
-    /* x > 0 < rows and y = 0 so no (y - 1) */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + y] +
-      board[(x - 1) * cols + (y + 1)] +
-      board[x * cols + (y + 1)] +
-      board[(x + 1) * cols + y] +
-      board[(x + 1) * cols + (y + 1)];
-    // clang-format on
-  }
-  if (x > 0 && x < (rows - 1) && y > 0 && y < (cols - 1)) {
-    /* x > 0 < rows and y > 0 < cols */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + (y - 1)] +
-      board[(x - 1) * cols + y] +
-      board[(x - 1) * cols + (y + 1)] +
-      board[x * cols + (y - 1)] +
-      board[x * cols + (y + 1)] +
-      board[(x + 1) * cols + (y - 1)] +
-      board[(x + 1) * cols + y] +
-      board[(x + 1) * cols + (y + 1)];
-    // clang-format on
-  }
-  if (x > 0 && x < (rows - 1) && y == (cols - 1)) {
-    /* x > 0 < rows and y = cols so no (y + 1) */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + (y - 1)] +
-      board[(x - 1) * cols + y] +
-      board[x * cols + (y - 1)] +
-      board[(x + 1) * cols + (y - 1)] +
-      board[(x + 1) * cols + y];
-    // clang-format on
-  }
-  if (x == (rows - 1) && y == 0) {
-    /* x = rows and y = 0 so no (x + 1) nor (y - 1) */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + y] +
-      board[(x - 1) * cols + (y + 1)] +
-      board[x * cols + (y + 1)];
-    // clang-format on
-  }
-  if (x == (rows - 1) && y > 0 && y < (cols - 1)) {
-    /* x = rows and y > 0 < cols so no (x + 1) */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + (y - 1)] +
-      board[(x - 1) * cols + y] +
-      board[(x - 1) * cols + (y + 1)] +
-      board[x * cols + (y - 1)] +
-      board[x * cols + (y + 1)];
-    // clang-format on
-  }
-  if (x == (rows - 1) && y == (cols - 1)) {
-    /* x = rows and y = cols so no (x + 1) nor (y + 1) */
-    // clang-format off
-    neighbours =
-      board[(x - 1) * cols + (y - 1)] +
-      board[(x - 1) * cols + y] +
-      board[x * cols + (y - 1)];
-    // clang-format on
-  }
+
   return neighbours;
 }
