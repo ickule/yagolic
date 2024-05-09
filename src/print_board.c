592ca0fd542cf3e4cfccebf0906db946a3ffc245
ickule <117572745+ickule@users.noreply.github.com>
2024-05-09 20:44:04 +0200

    :zap: Use the cells array to store groups instead of having a dedicated visited array

--- src/print_board.c
+++ src/print_board.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 #include "board.h"
-#include "null_pointer_check.h"
 
 /* ANSI escape codes for coloured output */
 const char *ANSI_COLOR_RESET = "\x1b[0m";
@@ -35,52 +34,52 @@ const char *get_colour(short group_number) {
 }
 
 /* Function to perform DFS traversal */
-void dfs(unsigned char x, unsigned char y, board_t board, short *visited, short group_number) {
+void dfs(unsigned char x, unsigned char y, board_t board, short group_number) {
   assert(board.cells != NULL);
   assert(x <= board.rows);
   assert(y <= board.cols);
-  assert(visited != NULL);
 
   /* Mark current cell as visited and assign group number */
-  visited[x * board.cols + y] = group_number;
+  board.cells[x * board.cols + y] = group_number;
 
   /* Iterate over neighbouring cells in a 3x3 grid around the current cell (x, y) */
   /* Loop variable type need to be able to hold 1 more value that the max of type for x and y*/
   for (short i = x - 1; i <= x + 1; ++i) {
     for (short j = y - 1; j <= y + 1; ++j) {
-      /* Check if the neighbor is within the bounds and is 1 and not visited */
-      if (i >= 0 && i < board.rows && j >= 0 && j < board.cols &&
-          board.cells[i * board.cols + j] == 1 && !visited[i * board.cols + j]) {
-        dfs(i, j, board, visited, group_number);
+      /* Check if the neighbour is within the bounds and is alive and not visited (i.e. equal to 1)
+       */
+      if (i >= 0 && i < board.rows && j >= 0 && j < board.cols && !(i == x && j == y) &&
+          board.cells[i * board.cols + j] == 1) {
+        dfs(i, j, board, group_number);
       }
     }
   }
 }
 
 /* Function to find connected cells and tagging them with a group number */
-void find_groups(board_t board, short *visited) {
+void find_groups(board_t board) {
   assert(board.cells != NULL);
-  assert(visited != NULL);
 
   /* Max group number on a 255*255 board is 16384*/
-  short group_number = 0;
+  /* 1 is reserved to indicate than a cell is alive and has not been visited so it cannot be used
+   * for group number */
+  short group_number = 1;
 
   for (unsigned char i = 0; i < board.rows; ++i) {
     for (unsigned char j = 0; j < board.cols; ++j) {
       /* If current cell is not visited and has value 1, increment group number and start DFS
        * traversal */
-      if (board.cells[i * board.cols + j] == 1 && !visited[i * board.cols + j]) {
+      if (board.cells[i * board.cols + j] == 1) {
         ++group_number;
-        dfs(i, j, board, visited, group_number);
+        dfs(i, j, board, group_number);
       }
     }
   }
 }
 
 /* Print the board with colours*/
-void print_cells_with_colours(board_t board, short *visited) {
+void print_cells_with_colours(board_t board) {
   assert(board.cells != NULL);
-  assert(visited != NULL);
 
   char representation;
 
@@ -93,7 +92,7 @@ void print_cells_with_colours(board_t board, short *visited) {
         /* but it does not matter as we are using an invisible character*/
         representation = ' ';
       }
-      printf("%s", get_colour(visited[i * board.cols + j]));
+      printf("%s", get_colour(board.cells[i * board.cols + j]));
       printf(" %c ", representation);
       printf("%s", ANSI_COLOR_RESET);
     }
@@ -104,14 +103,7 @@ void print_cells_with_colours(board_t board, short *visited) {
 void print_board(board_t board) {
   assert(board.cells != NULL);
 
-  /* visited will be used to store the group number */
-  /* and the fact that the cell has been visited already by the DFS */
-  short *visited = calloc(board.rows * board.cols, sizeof(short));
-  NULL_POINTER_CHECK(visited);
+  find_groups(board);
 
-  find_groups(board, visited);
-
-  print_cells_with_colours(board, visited);
-
-  free(visited);
+  print_cells_with_colours(board);
 }
